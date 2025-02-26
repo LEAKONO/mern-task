1f3dac2c7e3dd0f8b1f01b9634a2befe9ae4b47c
LEAKONO <leakonoemmanuel3@gmail.com>
2025-02-26 23:11:00 +0300

    modified code

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/tasks", require("./routes/taskRoute"));
+app.use("/", require("./routes/taskRoute"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
